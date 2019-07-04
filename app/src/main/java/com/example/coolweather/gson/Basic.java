ff13407ae008e08a32d3a8d399eb89768f132131
sd-danzi <840403192@qq.com>
2019-07-04 23:30:23 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/example/coolweather/gson/Basic.java
@@ -0,0 +1,15 @@
+package com.example.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Basic {
+    @SerializedName("city")
+    public String cityName;
+    @SerializedName("id")
+    public  String weather;
+    public Update update;
+    public  class Update{
+        @SerializedName("loc")
+        public String updateTime;
+    }
+}
